559018296160b5254448778034cafdd7ba9905d0
Grazelyn Rabe <Cloud9XHershey>
2023-05-09 21:26:08 +0800

    Deployed website

--- firebase-react-crud-main/src/firebase-config.js
+++ firebase-react-crud-main/src/firebase-config.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBG_9s2JJDUeBLtMenyPtIBsVLupa8vRB8",
-  authDomain: "fir-tutorial-ad573.firebaseapp.com",
-  projectId: "fir-tutorial-ad573",
-  storageBucket: "fir-tutorial-ad573.appspot.com",
-  messagingSenderId: "459866772432",
-  appId: "1:459866772432:web:b029523d67a8c9f1981fd5",
-  measurementId: "G-2R62T7YE0E",
-};
+    apiKey: "AIzaSyCbvAR5HBmjqUeSPQggRdGYgCBLIYT2Ypw",
+    authDomain: "fir-tutorial-2e36d.firebaseapp.com",
+    projectId: "fir-tutorial-2e36d",
+    storageBucket: "fir-tutorial-2e36d.appspot.com",
+    messagingSenderId: "190055495101",
+    appId: "1:190055495101:web:ee42eb4e9cc5fa5f40ced4",
+    measurementId: "G-QM06G57WP9"
+  };
 
-const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+  export const db = getFirestore(app);
